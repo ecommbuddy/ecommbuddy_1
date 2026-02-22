d56e38a172c6f50db4c3390d8e3b8a862c3b167f
Vansh Mishra <mishravansh831@gmail.com>
2026-02-22 13:29:21 +0530

    config file changed again

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ecommbuddy_1/',
+  base: '/',
   plugins: [react()],
   build: {
     target: 'esnext',
